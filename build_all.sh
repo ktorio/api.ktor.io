82f2ae9f27e82657fc02e9da44df27511b51f9ac
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-11-19 15:06:01 +0300

    Update version to 1.0.0

--- build_all.sh
+++ build_all.sh
@@ -2,8 +2,5 @@
 ./build_doc.sh 0.9.3 && \
 ./build_doc.sh 0.9.4 && \
 ./build_doc.sh 0.9.5 && \
-./build_doc.sh 1.0.0-alpha-1 \
-./build_doc.sh 1.0.0-beta-1 \
-./build_doc.sh 1.0.0-beta-2 \
-./build_doc.sh 1.0.0-beta-3
+./build_doc.sh 1.0.0
 
