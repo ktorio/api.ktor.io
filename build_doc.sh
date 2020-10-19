8349ea9b3bc423c4280ef9c258056d8bee139be7
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2020-10-19 21:32:03 +0300

    KTOR-1137 Make build script work with artifact dependencies

--- build_doc.sh
+++ build_doc.sh
@@ -1,93 +1,61 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
-if [ "$#" -ne 1 ]; then
-	echo "./build_doc.sh <ktor_version>"
-	exit 1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+KTOR_VERSION=$1
+DOKKA_DIR=$2
+
+if [ -z "${KTOR_VERSION}" ] || [ -z "${DOKKA_DIR}" ]; then
+  echo "Usage: $(basename "$0") version dokka-directory"
+  exit 1
 fi
 
-KTOR_VERSION=$1
-CNAME=`cat CNAME`
+DOMAIN="$(cat "${DIR}/CNAME")"
+WORK_DIR="output"
 
-echo "Building KTOR_VERSION=$KTOR_VERSION..."
+# Prepare working directory
+cp -rf "${DIR}/template" "${WORK_DIR}"
+cat > "${WORK_DIR}/_config.yml" <<- EOF
 
-API_DOC_FOLDER="$PWD/doc-output"
-
-#######################################################################
-
-JEKYLL_CONFIG="$(cat <<-EOF
-
-# Jekyll configuration file
 title: "Ktor $KTOR_VERSION"
 description: Asynchronous framework for web applications
-url: "https://$CNAME"
+url: "https://$DOMAIN"
 baseurl: /$KTOR_VERSION/
 
-# Dirs
 source: .
 destination: ../$KTOR_VERSION
 
 ktor_version: $KTOR_VERSION
 
-# Build settings
 markdown: kramdown
 exclude:
   - Gemfile
   - Gemfile.lock
 plugins:
   - jekyll-sitemap
+
 EOF
-)"
 
-#######################################################################
-rm -rf "$KTOR_VERSION"
-rm -rf doc-output
-cp -rf template doc-output
-#unzip template.zip -d doc-output
+cp -rf "${DOKKA_DIR}"/* "${WORK_DIR}"
 
-echo "$KTOR_VERSION"
-echo "$JEKYLL_CONFIG" > doc-output/_config.yml
+# Prepare result directory
+RESULT_DIR="${KTOR_VERSION}"
+mkdir -p "${RESULT_DIR}"
 
-if [ ! -d ktor  ]; then
-    git clone https://github.com/ktorio/ktor.git
-fi
+USER="$(id -u)"
+GROUP="$(id -g)"
 
-pushd ktor
+# Generate files via Jekyll
+docker run -v "${DIR}:/srv/jekyll" -v "${DIR}/vendor/bundle:/usr/local/bundle" -w "/srv/jekyll" jekyll/jekyll chown -R jekyll:jekyll "${RESULT_DIR}"
+docker run -v "${DIR}:/srv/jekyll" -v "${DIR}/vendor/bundle:/usr/local/bundle" -w "/srv/jekyll/${WORK_DIR}" jekyll/jekyll chown -R jekyll:jekyll .
+docker run -v "${DIR}:/srv/jekyll" -v "${DIR}/vendor/bundle:/usr/local/bundle" -w "/srv/jekyll/${WORK_DIR}" jekyll/jekyll jekyll b
+docker run -v "${DIR}:/srv/jekyll" -v "${DIR}/vendor/bundle:/usr/local/bundle" -w "/srv/jekyll" jekyll/jekyll chown -R "${USER}:${GROUP}" "${RESULT_DIR}"
+docker run -v "${DIR}:/srv/jekyll" -v "${DIR}/vendor/bundle:/usr/local/bundle" -w "/srv/jekyll/${WORK_DIR}" jekyll/jekyll chown -R "${USER}:${GROUP}" .
 
-git reset --hard
-git checkout master
-git fetch --all
-git reset --hard origin/master
+rm -f "${KTOR_VERSION}/index.yml"
 
-git reset --hard
-git checkout master
-git pull https://github.com/ktorio/ktor.git master
-git reset --hard
-git checkout "$KTOR_VERSION"
+java -jar tools/apidoc-indexer.jar "${KTOR_VERSION}"
 
-./gradlew dokkaWebsite
+docker run -i -v "${DIR}:/srv/work" -w "/srv/work" holgerbrandl/kscript - < "${DIR}/sync.kts"
 
-popd
-
-cp -rf ktor/apidoc/* "$API_DOC_FOLDER"
-
-# create site output and chmod so docker will be able to write into in
-mkdir -p "$KTOR_VERSION"
-chmod a+rwx "$KTOR_VERSION"
-
-USER_ID=`id -u`
-USER_GROUP=`id -g`
-
-# jekyll b
-docker run -v "$PWD:/srv/jekyll" -v "$PWD/vendor/bundle:/usr/local/bundle" -w "/srv/jekyll/doc-output" -it jekyll/jekyll jekyll b $* 
-
-# change generated files' owner
-docker run -v "$PWD:/srv/jekyll" -v "$PWD/vendor/bundle:/usr/local/bundle" -w "/srv/jekyll" -it jekyll/jekyll chown -R "$USER_ID:$USER_GROUP" "$KTOR_VERSION"
-
-# revoke permissions to the directory (non-recursive)
-chmod og-w "$KTOR_VERSION"
-
-rm -f $KTOR_VERSION/index.yml
-
-java -jar tools/apidoc-indexer.jar $KTOR_VERSION
-
+rm -rf "${WORK_DIR}"
